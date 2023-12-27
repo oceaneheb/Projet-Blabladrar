38b4146b38099ee6d9ebe76f77fbcea94599630d
Océane Hebrard <oceane47.heb@gmail.com>
2023-12-27 12:03:51 +0100

    Initialisation du projet, installation de REACT

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      'Bonjour les gens'
+    </>
+  )
+}
+
+export default App
